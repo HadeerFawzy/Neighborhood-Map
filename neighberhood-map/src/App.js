7f506e01b58e0fcb14d43fc8ff4d902d8f72c8d1
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-05-30 14:30:16 +0200

    try to add the map

--- neighberhood-map/src/App.js
+++ neighberhood-map/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import { Route } from 'react-router-dom'
+import {GoogleApiWrapper} from 'google-maps-react';
+import {ScriptCache} from './lib/ScriptCache';
+import GoogleApi from './lib/GoogleApi';
+import GoogleApiComponent from './lib/GoogleApiComponent';
+import Map from './Map.js'
 import './App.css';
 
 class App extends Component {
+  
   render() {
+    /*styles for map*/
+    const style = {
+      width: '100vw',
+      height: '100vh'
+    }
+
+    /*if map still loading*/
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <div style={style}>
+          <Map google={this.props.google}/>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default GoogleApiComponent({
+  apiKey: AIzaSyDUbTRPYZCp2Af2RSRwRfFsL6iK1iHyRG0
+})(App)
+
